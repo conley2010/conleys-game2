172bc6edb33ecd83bf86ce70a0bd282937e6f036
conley2010 <94207312+conley2010@users.noreply.github.com>
2021-11-16 18:45:32 -0600

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -28,7 +28,7 @@ let mapSprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 `)
 scene.setBackgroundColor(2)
-let startscreen = "welcome to Conleys game!"
+let startscreen = "welcome to Bobers great escape!"
 let subtitle = " ''map 2 made by Bryce'' "
 game.splash(startscreen,subtitle)
 let controls= "controls up=superjump"
@@ -47,42 +47,9 @@ let number_of_jumps: number
 number_of_jumps += 1
 let attack: number
 scene.setBackgroundColor(10)
-let mySprite2 = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, attack)
-let mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . .
-    . . . . f f f 2 2 f f f . . . .
-    . . . f f f 2 2 2 2 f f f . . .
-    . . f f f e e e e e e f f f . .
-    . . f f e 2 2 2 2 2 2 e e f . .
-    . . f e 2 f f f f f f 2 e f . .
-    . . f f f f e e e e f f f f . .
-    . f f e f b f 4 4 f b f e f f .
-    . f e e 4 1 f d d f 1 4 e e f .
-    . . f e e d d d d d d e e f . .
-    . . . f e e 4 4 4 4 e e f . . .
-    . . e 4 f 2 2 2 2 2 2 f 4 e . .
-    . . 4 d f 2 2 2 2 2 2 f d 4 . .
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
-    . . . . . f f f f f f . . . . .
-    . . . . . f f . . f f . . . . .
-`)
+let mySprite = sprites.create(assets.image`supersprite`, attack)
+
+
     level=0
 mySprite.setKind(SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
@@ -129,6 +96,7 @@ if (level ==0 ){
 
         tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
     }
+    powerjump = 2
 })
 scene.onOverlapTile(SpriteKind.attack, img`
     5 4 4 5 5 4 4 4 4 2 2 2 4 4 4 4
@@ -165,6 +133,8 @@ scene.onOverlapTile(SpriteKind.attack, img`
 
         tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
     }
+
+    powerjump=2
 })
 
 
@@ -777,7 +747,13 @@ scene.onOverlapTile(SpriteKind.Player, img`
     . . . . . . . . . . . . . . d d
 `, function(sprite: Sprite, location: tiles.Location) {
 level += 1
-pause(200)
+if (level == 3){
+    mySprite.setFlag(SpriteFlag.Ghost, true)
+mySprite.ay=0
+
+    controller.moveSprite(mySprite, 250, 250)
+}
+
 if (level == 1){
       tiles.setTilemap(tilemap`level4`)
   
@@ -789,7 +765,10 @@ if (level == 2) {
 
     tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
 
-}
+} 
+
+
+
         myMinimap = minimap.minimap(MinimapScale.Sixteenth, 1, 0)
         mapSprite.setFlag(SpriteFlag.RelativeToCamera, true)
 
@@ -1064,4 +1043,18 @@ game.onUpdateInterval(100, function() {
   )   
   
 
+scaling.scale3x(assets.image`supersprite`)
+mySprite.setImage(assets.image`supersprite`)
 
+forever(function() {
+ 
+    music.playMelody("E F G D C c B G F E D C C E F G D D ", 90)
+    music.playMelody("D D G F E C C D E F G B C C D G F E", 120)
+
+    
+    })
+
+
+
+
+    music.setVolume(34)
